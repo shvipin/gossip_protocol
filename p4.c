d2dd31754713b9b3465052157807697c3d8d10e1
David Zager <djzager@e2-1003-210-l-c.eos.ncsu.edu>
2015-11-11 22:57:35 -0500

    Start the initialization portion

--- p4.c
+++ p4.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
+#include <pthread.h>
 
+#define ENDPOINTS "endpoints"
+#define NUM_THREADS 2
+#define TRUE 1
+#define FALSE 0
+
+void server();
+void client();
+
+// user provided arguments
 int num_nodes;
 int gossip_b;
 int gossip_c;
@@ -9,9 +20,19 @@ int num_failure_nodes;
 int time_bw_failures;
 int random_seed;
 
+// our globals
+typedef struct node {
+} node_t;
+node *endpoints; 
+int last_process = FALSE;
+FILE *ep;
+pthread_t server_thread;
+pthread_barrier_t barrier;
+
 int main(int argc, const char *argv[])
 {
   if (argc != 8) {
+    // user messed up. Print usage info and exit
     printf("Usage ./p4 N b c F B P S\n \
       number of peer nodes N\n \
       gossip parameter b\n \
@@ -23,6 +44,7 @@ int main(int argc, const char *argv[])
     exit(1);
   }
 
+  // take in user's arguments
   num_nodes = atoi(argv[1]);
   gossip_b = atoi(argv[2]);
   gossip_c = atoi(argv[3]);
@@ -31,6 +53,34 @@ int main(int argc, const char *argv[])
   time_bw_failures = atoi(argv[6]);
   random_seed = atoi(argv[7]);
 
+  // allocate memory for our endpoints
+  endpoints = (node *)malloc(num_nodes * sizeof(node_t));
+  ep = fopen(ENDPOINTS, "w+"):  
 
+  // dispatch server thread
+  pthread_barrier_init(&barrier, NULL, NUM_THREADS);
+  pthread_create(&server_thread, 0, NULL, &server, NULL);
+
+  // wait
+  pthread_barrier_wait(&barrier);
+  client();
+
+  fclose(fp);
   return 0;
+}
+
+
+void client()
+{
+  if (last_process) {
+
+  } else {
+
+  }
+}
+
+
+void server()
+{
+
 }
